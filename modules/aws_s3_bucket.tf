f57b27fa3b4926f0af3ad56a3a692160a8bc89c3
梶田英完 <hidemasa_kajita@respawn.jp>
2021-10-16 21:51:25 +0900

    add s3 and cloudfront

--- modules/aws_s3_bucket.tf
+++ modules/aws_s3_bucket.tf
@@ -1,11 +1,39 @@
 // policy, cors の設定が必要
 resource "aws_s3_bucket" "bucket" {
-  bucket = "tfbucketmcdlknvejk8u"
+  bucket_prefix = "static-www"
   acl    = "private"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  } 
 
   tags = {
     Name      = "bucket"
     Env       = "${var.env}"
     CreatedBy = "${var.created_by}"
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket" {
+    bucket = aws_s3_bucket.bucket.id
+    policy = data.aws_iam_policy_document.static-www.json
+}
+
+data "aws_iam_policy_document" "static-www" {
+  statement {
+    sid = "Allow CloudFront"
+    effect = "Allow"
+    principals {
+        type = "AWS"
+        identifiers = [aws_cloudfront_origin_access_identity.static-www.iam_arn]
+    }
+    actions = [
+        "s3:GetObject"
+    ]
+
+    resources = [
+        "${aws_s3_bucket.bucket.arn}/*"
+    ]
   }
 }
